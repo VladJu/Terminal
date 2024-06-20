1dd4e06b5ae3ded5fb72cf2fb6371b8c3c964646
VladJu <juikovvlad@gmail.com>
2024-06-20 17:03:39 +0300

    Add Scaling

--- app/src/main/java/com/example/terminal/data/ApiService.kt
+++ app/src/main/java/com/example/terminal/data/ApiService.kt
@@ -4,6 +4,6 @@ import retrofit2.http.GET
 
 interface ApiService {
 
-    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-02-09?adjusted=true&sort=asc&limit=49999&apiKey=DqBoaSBY9WnId0vQAIo1MMep5ZpVmsGE")
+    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-02-09?adjusted=true&sort=dec&limit=49999&apiKey=DqBoaSBY9WnId0vQAIo1MMep5ZpVmsGE")
     suspend fun loadBars() : Result
 }
